4689daee6e7d607efce271b1e7bb2da3b1d69d80
Abhishek <abhishek404singh@gmail.com>
2021-12-27 23:33:06 +0530

    broken homepage into smaller components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { HomePage } from './homepage.component';
-import './App.css';
+import { HomePage } from './components/pages/homepage/homepage.component';
+import './App.css'
 
 function App(){
   return (
