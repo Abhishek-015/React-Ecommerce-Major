56d8a64ba4d32cc027ad0cbe92c758aecfce1e19
Abhishek <abhishek404singh@gmail.com>
2022-01-02 14:33:08 +0530

    implemented firebase utils completely that includes user authentication on our firebase database

--- src/App.js
+++ src/App.js
@@ -28,13 +28,14 @@ class App extends React.Component {
                  id:snapShot.id,
                  ...snapShot.data()
                }
-             })
+             },()=>console.log(this.state))
            })
-           console.log(this.state)
          }else{
            this.setState({currentUser:userAuth})
-         }
-       })
+          }
+
+        })
+         
      }
 
      componentWillUnmount(){
