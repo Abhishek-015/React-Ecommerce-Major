2e5ab03562cf8f0edf52457dd25c7666379b8a28
Abhishek <abhishek404singh@gmail.com>
2022-01-10 13:29:07 +0530

    updated the cart data with some new data,added cart utils functionality with cart reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { connect } from "react-redux";
 import CustomButton from "../custom-button/custum-button.component";
+import CartItem from "../cart-item/cart-item.component";
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = () =>(
+const CartDropdown = ({cartItems}) =>(
     <div className="cart-dropdown">
         <div className="cart-items"/>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropdown
+const mapStateToProps = ({cart : {cartItems}})=>({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown)
